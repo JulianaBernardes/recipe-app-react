e7fe99182ee72e94edbd8d485ad2766a3b8ed25b
Juliana Bernardes Pereira <julianabp96@gmail.com>
2020-11-30 21:35:26 +0000

    recipe style and ingredients

--- src/App.js
+++ src/App.js
@@ -29,6 +29,7 @@ const App = () => {
   const getSearch = e => {
     e.preventDefault()
     setQuery(search)
+    setSearch('')
   }
 
   return (
@@ -44,14 +45,18 @@ const App = () => {
           Search
         </button>
       </form>
-      {recipes.map(recipe => (
+      <div className="recipes">
+        {recipes.map(recipe => (
         <Recipe
-          key={recipe.recipe.label}
+          key={recipe.recipe.calories}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
         />
       ))}
+      </div>
+      
     </div>
   )
 }
